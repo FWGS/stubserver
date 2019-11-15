02dd75cc8a57c836fc49101616538f2986445401
mittorn <mittorn@sibmail.com>
2019-11-15 23:17:44 +0700

    client stub

---
+++ client/int.c
@@ -0,0 +1,166 @@
+
+
+void Initialize( void )
+{
+}
+
+void HUD_Init( void )
+{
+}
+
+void HUD_VidInit( void )
+{
+}
+void HUD_Shutdown( void )
+{
+}
+void HUD_Redraw( void )
+{
+}
+
+void HUD_UpdateClientData( void )
+{
+}
+void HUD_Reset( void )
+{
+}
+
+void HUD_PlayerMove( void )
+{
+}
+
+void HUD_PlayerMoveInit( void )
+{
+}
+
+void HUD_PlayerMoveTexture( void )
+{
+}
+
+void HUD_ConnectionlessPacket( void )
+{
+}
+
+void HUD_GetHullBounds( void )
+{
+}
+
+void HUD_Frame( void )
+{
+}
+
+void HUD_PostRunCmd( void )
+{
+}
+
+void HUD_Key_Event( void )
+{
+}
+
+void HUD_AddEntity( void )
+{
+}
+
+void HUD_CreateEntities( void )
+{
+}
+
+void HUD_StudioEvent( void )
+{
+}
+
+void HUD_TxferLocalOverrides( void )
+{
+
+}
+
+void HUD_ProcessPlayerState( void )
+{
+
+}
+
+void HUD_TxferPredictionData( void )
+{
+
+}
+
+void HUD_TempEntUpdate( void )
+{
+
+}
+
+void HUD_DrawNormalTriangles( void )
+{
+
+}
+
+void HUD_DrawTransparentTriangles( void )
+{
+
+}
+
+void HUD_GetUserEntity( void )
+{
+
+}
+
+void Demo_ReadBuffer( void )
+{
+
+}
+
+void CAM_Think( void )
+{
+
+}
+
+void CL_IsThirdPerson( void )
+{
+
+}
+
+void CL_CameraOffset( void )
+{
+
+}
+
+void CL_CreateMove( void )
+{
+
+}
+
+void IN_ActivateMouse( void )
+{
+
+}
+
+void IN_DeactivateMouse( void )
+{
+
+}
+
+void IN_MouseEvent( void )
+{
+
+}
+
+void IN_Accumulate( void )
+{
+
+}
+
+void IN_ClearStates( void )
+{
+
+}
+
+void V_CalcRefdef( void )
+{
+
+}
+
+void KB_Find( void )
+{
+
+}
+
